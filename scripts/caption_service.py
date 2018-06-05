5317da4effa5e4bfa6b1e506cf4c3f9902a5d71b
Brandon Trabucco <brandon@btrabucco.com>
2018-06-05 15:20:32 -0400

    Add files via upload

--- scripts/caption_service.py
+++ scripts/caption_service.py
@@ -4,11 +4,13 @@ import rospy
 from image_caption_machine.srv import Caption, CaptionResponse
 from captioning_utils import run_caption
 
+import numpy as np
+
 class CaptionService(object):
 
     def __init__(self):
         rospy.init_node("caption_service_node")
-        print("Caption service started.")
+        rospy.loginfo("Caption service started.")
         self.service = rospy.Service(
             "caption_service", 
             Caption, 
@@ -17,7 +19,11 @@ class CaptionService(object):
 
     def handle_caption(self, request):
         image = request.image_buffer
-        return CaptionResponse(run_caption(image))
+        rospy.loginfo("Received captioning request.")
+        data = np.array(image.flat_buffer).reshape(
+                (image.height, image.width, image.depth))
+        return CaptionResponse(run_caption(data))
 
 if __name__ == "__main__":
     cs = CaptionService()
+
--- scripts/simulate_camera.py
+++ scripts/simulate_camera.py
@@ -4,31 +4,30 @@ import rospy
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 
+import numpy as np
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
 class SimulateCamera(object):
 
-    def __init__(self):
+    def __init__(self, h=299, w=299, c=3):
         rospy.init_node("simulate_camera_node")
-        self.publisher = rospy.Publisher("sensors/rgb/image_raw", Image, queue_size=10)
+        self.publisher = rospy.Publisher(
+            "camera/rgb/image_raw", Image, queue_size=10)
         self.rate = rospy.Rate(29)
-        self.h = Header()
-        self.h.seq = 0
-        self.h.stamp = rospy.get_rostime()
-        self.h.frame_id = "usb_cam"
-        self.image = Image()
-        self.image.header = self.h
-        self.image.height = 10
-        self.image.width = 10
-        self.image.encoding = "rgb8"
-        self.image.is_bigendian = 0
-        self.image.step = 10
-        self.image.data = [125 for i in range(100)]
-        self.spin()
+        self.bridge = CvBridge()
+        self.spin(h, w, c)
 
-    def spin(self):
-        print("Starting camera simulation.")
+    def spin(self, height, width, channels):
+        rospy.loginfo("Starting camera simulation.")
         while not rospy.is_shutdown():
-            self.publisher.publish(self.image)
+            image = self.bridge.cv2_to_imgmsg(
+                np.random.randint(0, 255, 
+                    (height, width, channels), dtype=np.uint8),
+                "bgr8")
+            self.publisher.publish(image)
             self.rate.sleep()
 
 if __name__ == "__main__":
     sc = SimulateCamera()
+
